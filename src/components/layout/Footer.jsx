abd11029029b97b38dff201fb28cd242dae3986d
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-23 17:45:55 +0700

    feat(homepage) : adding footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-dark text-white text-center py-3 fs-normal fw-bold">
+      Copyright © 2025 Traveloka. All rights reserved{" "}
+    </div>
+  );
 };
 
 export default Footer;
