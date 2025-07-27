059a2dd07698b018fa737525cd9f7d7992410eaa
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-27 16:12:53 +0700

    feat(slice) : adding authslice for auth

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -1,5 +1,54 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
 const initialState = {
   token: localStorage.getItem("token") || null,
   loading: null,
   error: null,
 };
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/v1/auth/login",
+        { email, password }
+      );
+      console.log(response);
+      localStorage.setItem("token", response.data.token);
+      return response.data.token;
+    } catch (error) {
+      return rejectWithValue(error.response.data.message);
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout(state) {
+      localStorage.removeItem("token");
+      state.token = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.token = action.payload;
+        state.loading = false;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+      });
+  },
+});
+
+export const { logout } = authSlice.actions;
+export default authSlice.reducer;
