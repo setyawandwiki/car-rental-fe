4d5756511ca6bf32e7da1400dc6093f5b4198991
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-29 17:13:54 +0700

    feat(app) : adding persist reducer on store js

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import cityReducer from "../features/citySlice";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    city: cityReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  city: cityReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export default store;
