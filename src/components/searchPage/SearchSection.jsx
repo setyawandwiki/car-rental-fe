d21d81eb8a10b635538fe1e97014f791b1ac2dfe
setyawandwiki <setyawandwiki1@gmail.com>
2025-07-30 19:01:57 +0700

    feat(searchsection) : showing data from search

--- src/components/searchPage/SearchSection.jsx
+++ src/components/searchPage/SearchSection.jsx
@@ -1,252 +1,88 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { NavLink } from "react-router";
 
 const SearchSection = () => {
   const companyCar = useSelector((state) => state.companyCar.data);
-  console.log(companyCar);
   return (
     <div className="container w-75">
       <div className="row">
-        <div className="col-12 rounded shadow my-3">
-          <div className="row py-3">
-            <div className="col-2">
-              <img
-                src="https://ik.imagekit.io/tvlk/image/imageResource/2023/01/26/1674730572253-02af9e48a2bc5b753259d9e71c433652.jpeg?tr=q-75,w-140"
-                alt=""
-                className="img-fluid w-100 h-100 object-fit-cover"
-              />
-            </div>
-            <div className="col-6">
-              <div className="title">
-                <h1 className="h6 fw-bold">
-                  Honda Genio (Pool Kuta, area Bandara)
-                </h1>
-              </div>
-              <div className="car__type d-flex align-items-center gap-2">
-                <img
-                  src="../src/assets/steer.svg"
-                  alt=""
-                  style={{ height: 24, width: 16 }}
-                />
-                <p className="fs-normal fw-normal1">Automatic</p>
-              </div>
-              <div className="description d-flex gap-3">
-                <div className="baggage d-flex gap-2 align-items-center">
-                  <img
-                    src="../src/assets/lugage.svg"
-                    alt=""
-                    style={{ height: 24, width: 16 }}
-                  />
-                  <p className="fs-normal fw-normal1">Automatic</p>
-                </div>
-                <div className="baggage d-flex gap-2 align-items-center">
-                  <img
-                    src="../src/assets/chair.svg"
-                    alt=""
-                    style={{ height: 24, width: 16 }}
-                  />
-                  <p className="fs-normal fw-normal1">Automatic</p>
-                </div>
-              </div>
-              <div className="driver__type pt-3">
-                <p className="fs-small">Tanpa Sopir</p>
-              </div>
-            </div>
-            <div className="col-4 text-end">
-              <div className="price__container h-100 d-flex flex-column align-items-end">
-                <p className="py-3 fw-bold" style={{ color: "#ff5e1f" }}>
-                  Rp. 70.000 / Dari Hari
-                </p>
-                <button
-                  className="btn btn-primary w-50"
-                  style={{ background: "#ff5e1f", border: "none" }}
-                >
-                  Lanjutkan
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 rounded shadow my-3">
-          <div className="row py-3">
-            <div className="col-2">
-              <img
-                src="https://ik.imagekit.io/tvlk/image/imageResource/2023/01/26/1674730572253-02af9e48a2bc5b753259d9e71c433652.jpeg?tr=q-75,w-140"
-                alt=""
-                className="img-fluid w-100 h-100 object-fit-cover"
-              />
-            </div>
-            <div className="col-6">
-              <div className="title">
-                <h1 className="h6 fw-bold">
-                  Honda Genio (Pool Kuta, area Bandara)
-                </h1>
-              </div>
-              <div className="car__type d-flex align-items-center gap-2">
-                <img
-                  src="../src/assets/steer.svg"
-                  alt=""
-                  style={{ height: 24, width: 16 }}
-                />
-                <p className="fs-normal fw-normal1">Automatic</p>
-              </div>
-              <div className="description d-flex gap-3">
-                <div className="baggage d-flex gap-2 align-items-center">
-                  <img
-                    src="../src/assets/lugage.svg"
-                    alt=""
-                    style={{ height: 24, width: 16 }}
-                  />
-                  <p className="fs-normal fw-normal1">Automatic</p>
-                </div>
-                <div className="baggage d-flex gap-2 align-items-center">
-                  <img
-                    src="../src/assets/chair.svg"
-                    alt=""
-                    style={{ height: 24, width: 16 }}
-                  />
-                  <p className="fs-normal fw-normal1">Automatic</p>
-                </div>
-              </div>
-              <div className="driver__type pt-3">
-                <p className="fs-small">Tanpa Sopir</p>
-              </div>
-            </div>
-            <div className="col-4 text-end">
-              <div className="price__container h-100 d-flex flex-column align-items-end">
-                <p className="py-3 fw-bold" style={{ color: "#ff5e1f" }}>
-                  Rp. 70.000 / Dari Hari
-                </p>
-                <button
-                  className="btn btn-primary w-50"
-                  style={{ background: "#ff5e1f", border: "none" }}
-                >
-                  Lanjutkan
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 rounded shadow my-3">
-          <div className="row py-3">
-            <div className="col-2">
-              <img
-                src="https://ik.imagekit.io/tvlk/image/imageResource/2023/01/26/1674730572253-02af9e48a2bc5b753259d9e71c433652.jpeg?tr=q-75,w-140"
-                alt=""
-                className="img-fluid w-100 h-100 object-fit-cover"
-              />
-            </div>
-            <div className="col-6">
-              <div className="title">
-                <h1 className="h6 fw-bold">
-                  Honda Genio (Pool Kuta, area Bandara)
-                </h1>
-              </div>
-              <div className="car__type d-flex align-items-center gap-2">
-                <img
-                  src="../src/assets/steer.svg"
-                  alt=""
-                  style={{ height: 24, width: 16 }}
-                />
-                <p className="fs-normal fw-normal1">Automatic</p>
-              </div>
-              <div className="description d-flex gap-3">
-                <div className="baggage d-flex gap-2 align-items-center">
-                  <img
-                    src="../src/assets/lugage.svg"
-                    alt=""
-                    style={{ height: 24, width: 16 }}
-                  />
-                  <p className="fs-normal fw-normal1">Automatic</p>
-                </div>
-                <div className="baggage d-flex gap-2 align-items-center">
-                  <img
-                    src="../src/assets/chair.svg"
-                    alt=""
-                    style={{ height: 24, width: 16 }}
-                  />
-                  <p className="fs-normal fw-normal1">Automatic</p>
-                </div>
-              </div>
-              <div className="driver__type pt-3">
-                <p className="fs-small">Tanpa Sopir</p>
-              </div>
-            </div>
-            <div className="col-4 text-end">
-              <div className="price__container h-100 d-flex flex-column align-items-end">
-                <p className="py-3 fw-bold" style={{ color: "#ff5e1f" }}>
-                  Rp. 70.000 / Dari Hari
-                </p>
-                <button
-                  className="btn btn-primary w-50"
-                  style={{ background: "#ff5e1f", border: "none" }}
-                >
-                  Lanjutkan
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 rounded shadow my-3">
-          <div className="row py-3">
-            <div className="col-2">
-              <img
-                src="https://ik.imagekit.io/tvlk/image/imageResource/2023/01/26/1674730572253-02af9e48a2bc5b753259d9e71c433652.jpeg?tr=q-75,w-140"
-                alt=""
-                className="img-fluid w-100 h-100 object-fit-cover"
-              />
-            </div>
-            <div className="col-6">
-              <div className="title">
-                <h1 className="h6 fw-bold">
-                  Honda Genio (Pool Kuta, area Bandara)
-                </h1>
-              </div>
-              <div className="car__type d-flex align-items-center gap-2">
-                <img
-                  src="../src/assets/steer.svg"
-                  alt=""
-                  style={{ height: 24, width: 16 }}
-                />
-                <p className="fs-normal fw-normal1">Automatic</p>
-              </div>
-              <div className="description d-flex gap-3">
-                <div className="baggage d-flex gap-2 align-items-center">
-                  <img
-                    src="../src/assets/lugage.svg"
-                    alt=""
-                    style={{ height: 24, width: 16 }}
-                  />
-                  <p className="fs-normal fw-normal1">Automatic</p>
-                </div>
-                <div className="baggage d-flex gap-2 align-items-center">
-                  <img
-                    src="../src/assets/chair.svg"
-                    alt=""
-                    style={{ height: 24, width: 16 }}
-                  />
-                  <p className="fs-normal fw-normal1">Automatic</p>
-                </div>
-              </div>
-              <div className="driver__type pt-3">
-                <p className="fs-small">Tanpa Sopir</p>
-              </div>
-            </div>
-            <div className="col-4 text-end">
-              <div className="price__container h-100 d-flex flex-column align-items-end">
-                <p className="py-3 fw-bold" style={{ color: "#ff5e1f" }}>
-                  Rp. 70.000 / Dari Hari
-                </p>
-                <button
-                  className="btn btn-primary w-50"
-                  style={{ background: "#ff5e1f", border: "none" }}
-                >
-                  Lanjutkan
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        {companyCar
+          ? companyCar.map((val) => {
+              return (
+                <>
+                  <div className="col-12 rounded shadow my-3">
+                    <div className="row py-3">
+                      <div className="col-2">
+                        <img
+                          src={`${val.car.image}`}
+                          alt=""
+                          className="img-fluid w-100 h-100 object-fit-cover"
+                        />
+                      </div>
+                      <div className="col-6">
+                        <div className="title">
+                          <h1 className="h6 fw-bold">
+                            {val.car.name} {`(${val.city})`}
+                          </h1>
+                        </div>
+                        <div className="car__type d-flex align-items-center gap-2">
+                          <img
+                            src="../src/assets/steer.svg"
+                            alt=""
+                            style={{ height: 24, width: 16 }}
+                          />
+                          <p className="fs-normal fw-normal1">{val.car_type}</p>
+                        </div>
+                        <div className="description d-flex gap-3">
+                          <div className="baggage d-flex gap-2 align-items-center">
+                            <img
+                              src="../src/assets/lugage.svg"
+                              alt=""
+                              style={{ height: 24, width: 16 }}
+                            />
+                            <p className="fs-normal fw-normal1">
+                              {val.car.baggages} baggages
+                            </p>
+                          </div>
+                          <div className="baggage d-flex gap-2 align-items-center">
+                            <img
+                              src="../src/assets/chair.svg"
+                              alt=""
+                              style={{ height: 24, width: 16 }}
+                            />
+                            <p className="fs-normal fw-normal1">
+                              {val.car.seats} seats
+                            </p>
+                          </div>
+                        </div>
+                        <div className="driver__type pt-3">
+                          <p className="fs-small">Tanpa Sopir</p>
+                        </div>
+                      </div>
+                      <div className="col-4 text-end">
+                        <div className="price__container h-100 d-flex flex-column align-items-end">
+                          <p
+                            className="py-3 fw-bold"
+                            style={{ color: "#ff5e1f" }}
+                          >
+                            Rp. {val.price} / Hari
+                          </p>
+                          <NavLink
+                            to={`/search/${val.id}`}
+                            className="btn btn-primary w-50"
+                            style={{ background: "#ff5e1f", border: "none" }}
+                          >
+                            Lanjutkan
+                          </NavLink>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })
+          : null}
       </div>
     </div>
   );
